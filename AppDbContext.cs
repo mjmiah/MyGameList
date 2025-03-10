8305e2da97329f006fee9914e935f3bb6bd0bb33
mjmiah <65255238+mjmiah@users.noreply.github.com>
2025-03-10 01:38:44 +0000

    Add files via upload

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -8,16 +8,35 @@ namespace MyGameList
     public class AppDbContext : DbContext
     {
         public IConfiguration _config { get; set; }
-        public AppDbContext(IConfiguration config) 
+        public AppDbContext(IConfiguration config)
         {
             _config = config;
         }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
         }
 
-        public DbSet<GameItem> GameItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<UserGame> UserGames { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserGame>()
+                .HasKey(ug => new { ug.userId, ug.gameId });  // composite key for UserGame (userId + gameId)
+
+            modelBuilder.Entity<UserGame>()
+                .HasOne<User>()  // relationship between User and UserGame
+                .WithMany()  // User can have many UserGames
+                .HasForeignKey(ug => ug.userId);
+
+            modelBuilder.Entity<UserGame>()
+                .HasOne<Game>()  // relationship between Game and UserGame
+                .WithMany()  // Game can have many UserGames
+                .HasForeignKey(ug => ug.gameId);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using MyGameList;
+using MyGameList.Helpers;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration); // make builder.Configuration available for all services
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddOpenApi(); // Add OpenAPI services to WebApplicationBuilder
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"))); // DbContext dependency injection
+builder.Services.AddScoped<AuthManager>(); // AuthManager dependency injection
+
 
 var app = builder.Build();
 
@@ -11,6 +22,13 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
+}
+else
+{
+    app.MapScalarApiReference();
+    app.MapOpenApi(); // Endpoint to the app to serve the OpenAPI document 
+
 }
 
 app.UseHttpsRedirection();
